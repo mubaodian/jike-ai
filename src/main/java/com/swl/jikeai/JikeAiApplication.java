70ba05fb9f25e04e065172bf88a8743860024d14
1350147451@qq.com <1350147451@qq.com>
2026-05-18 12:56:42 +0800

    初始化后端项目

---
+++ src/main/java/com/swl/jikeai/JikeAiApplication.java
@@ -0,0 +1,13 @@
+package com.swl.jikeai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JikeAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JikeAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swl/jikeai/JikeAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swl.jikeai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JikeAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
