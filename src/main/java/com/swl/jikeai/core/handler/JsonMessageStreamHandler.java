381a28b315271266701ea32241d19f00b2cf6bdf
1350147451@qq.com <1350147451@qq.com>
2026-07-21 17:49:59 +0800

    工程项目生成（工程项目构建、浏览和部署）- 后端开发 1. 创建项目构建器（包含异步构建） 2. 在 AI 回复完毕后执行构建代码 3. 在 AppService 中增加 Vue 项目部署逻辑

--- src/main/java/com/swl/jikeai/core/handler/JsonMessageStreamHandler.java
+++ src/main/java/com/swl/jikeai/core/handler/JsonMessageStreamHandler.java
@@ -5,15 +5,19 @@ import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.swl.jikeai.ai.model.message.*;
+import com.swl.jikeai.core.builder.VueProjectBuilder;
 import com.swl.jikeai.model.entity.User;
 import com.swl.jikeai.model.enums.ChatHistoryMessageTypeEnum;
 import com.swl.jikeai.service.ChatHistoryService;
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static com.swl.jikeai.constant.AppConstant.CODE_OUTPUT_ROOT_DIR;
 
 /**
  * JSON 消息流处理器
@@ -22,6 +26,9 @@ import java.util.Set;
 @Slf4j
 @Component
 public class JsonMessageStreamHandler {
+
+    @Resource
+    private VueProjectBuilder vueProjectBuilder;
 
     /**
      * 处理 TokenStream（VUE_PROJECT）
@@ -50,6 +57,9 @@ public class JsonMessageStreamHandler {
                     // 流式响应完成后，添加 AI 消息到对话历史
                     String aiResponse = chatHistoryStringBuilder.toString();
                     chatHistoryService.addChatMessage(appId, aiResponse, ChatHistoryMessageTypeEnum.AI.getValue(), loginUser.getId());
+                    // 异步构造 Vue 项目
+                    String projectPath = CODE_OUTPUT_ROOT_DIR + "/vue_project_" + appId;
+                    vueProjectBuilder.buildProjectAsync(projectPath);
                 })
                 .doOnError(error -> {
                     // 如果AI回复失败，也要记录错误消息
--- src/main/java/com/swl/jikeai/service/impl/AppServiceImpl.java
+++ src/main/java/com/swl/jikeai/service/impl/AppServiceImpl.java
@@ -10,6 +10,7 @@ import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.spring.service.impl.ServiceImpl;
 import com.swl.jikeai.ai.AiCodeGeneratorService;
 import com.swl.jikeai.core.AiCodeGeneratorFacade;
+import com.swl.jikeai.core.builder.VueProjectBuilder;
 import com.swl.jikeai.core.handler.StreamHandlerExecutor;
 import com.swl.jikeai.exception.BusinessException;
 import com.swl.jikeai.exception.ErrorCode;
@@ -61,6 +62,8 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App> implements AppSe
     private AiCodeGeneratorFacade aiCodeGeneratorFacade;
     @Resource
     private StreamHandlerExecutor streamHandlerExecutor;
+    @Resource
+    private VueProjectBuilder vueProjectBuilder;
 
 
     @Override
@@ -112,6 +115,18 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App> implements AppSe
         // 检查源目录是否存在
         File sourceDir = new File(sourceDirPath);
         ThrowUtils.throwIf(!sourceDir.exists() || !sourceDir.isDirectory(), ErrorCode.SYSTEM_ERROR, "应用代码不存在，请先生成代码");
+        // Vue 项目特殊处理：执行构建
+        CodeGenTypeEnum codeGenTypeEnum = CodeGenTypeEnum.getEnumByValue(codeGenType);
+        if(codeGenTypeEnum == CodeGenTypeEnum.VUE_PROJECT){
+            // 构建
+            boolean buildSuccess = vueProjectBuilder.buildProject(sourceDirPath);
+            ThrowUtils.throwIf(!buildSuccess, ErrorCode.SYSTEM_ERROR, "Vue项目构建失败，请稍后重试");
+            // 检查 dist 目录是否存在
+            File distDir = new File(sourceDirPath, "dist");
+            ThrowUtils.throwIf(!distDir.exists() || !distDir.isDirectory(), ErrorCode.SYSTEM_ERROR, "Vue 项目构建完成但未生成 dist 目录");
+            // 将 dist 目录重命名为源目录
+            sourceDir = distDir;
+        }
         // 复制文件代码到部署目录
         String deployDirPath = CODE_DEPLOY_ROOT_DIR + File.separator + deployKey;
         try {
