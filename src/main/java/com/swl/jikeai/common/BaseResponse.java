d3045f062694a4ad7d2a6fe1570814ca294fb3c2
1350147451@qq.com <1350147451@qq.com>
2026-09-14 16:28:55 +0800

    系统优化-性能优化 --- Redis 缓存优化，加上Value JSON序列化器

--- src/main/java/com/swl/jikeai/common/BaseResponse.java
+++ src/main/java/com/swl/jikeai/common/BaseResponse.java
@@ -2,10 +2,12 @@ package com.swl.jikeai.common;
 
 import com.swl.jikeai.exception.ErrorCode;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
+@NoArgsConstructor
 public class BaseResponse<T> implements Serializable {
 
     private int code;
--- src/main/java/com/swl/jikeai/config/RedisCacheManagerConfig.java
+++ src/main/java/com/swl/jikeai/config/RedisCacheManagerConfig.java
@@ -1,7 +1,5 @@
 package com.swl.jikeai.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.annotation.Resource;
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -22,21 +21,20 @@ public class RedisCacheManagerConfig {
 
     @Bean
     public CacheManager cacheManager() {
-        // 配置 ObjectMapper 支持 Java8 时间类型
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        
         // 默认配置
         RedisCacheConfiguration defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(30)) // 默认 30 分钟过期
                 .disableCachingNullValues() // 禁用 null 值缓存
                 // key 使用 String 序列化器
                 .serializeKeysWith(RedisSerializationContext.SerializationPair
-                        .fromSerializer(new StringRedisSerializer()));
-//                // value 使用 JSON 序列化器（支持复杂对象）
-//                .serializeValuesWith(RedisSerializationContext.SerializationPair
-//                        .fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper)));
-        
+                        .fromSerializer(new StringRedisSerializer()))
+                // value 使用 JSON 序列化器（支持复杂对象和Java8时间类型，Jackson 3 自动支持）
+                // 启用 unsafe default typing 保证反序列化能够正确还原原始类型
+                .serializeValuesWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(GenericJacksonJsonRedisSerializer.create(mapper ->
+                                mapper.enableSpringCacheNullValueSupport()
+                                      .enableUnsafeDefaultTyping())));
+
         return RedisCacheManager.builder(redisConnectionFactory)
                 .cacheDefaults(defaultConfig)
                 // 针对 good_app_page 配置5分钟过期
